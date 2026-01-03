a3549d10fae1f119c59a8b62b42c9d128ba0393f
Sam J <sam_joseph@live.com>
2026-01-03 09:34:28 -0800

    created the delete function and tried shorthand version for the first time

--- logicEngine.js
+++ logicEngine.js
@@ -14,4 +14,33 @@ function pushingToDo(item){
     return toDoObject
 }
 
-module.exports = {pushingToDo, todo}
+function deleteNote(id){
+    /* here I will try the expanded form then I will try the shorthand inorder
+    to get a good grasp about the concept */
+    
+    /*
+    let itemIndex = -1;
+    for(let i = 0; i < todo.length; i++){
+        let t = todo[i];
+        if (t.id === id){
+            itemIndex = i;
+            break;
+        }
+        
+    }
+        */
+
+    //short hand for the above version
+
+
+    const itemIndex = todo.findIndex(t => t.id === id);
+    // here findIndex will assign -1 if the id is not in the todo list
+    if(itemIndex === -1){
+        throw new Error(`Todo with id ${id} not found`);
+    }
+
+    return todo.splice(itemIndex,1);
+    
+}
+
+module.exports = {pushingToDo, todo, deleteNote}
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {pushingToDo, todo} = require("./logicEngine");
+const {pushingToDo, todo, deleteNote} = require("./logicEngine");
 
 const app = express();
 app.use(express.json());
@@ -18,5 +18,15 @@ app.post("/todo",(req, res) => {
 });
 
 app.get("/todo",(req, res) =>{
-    res.json(todo)
+    res.json(todo);
+});
+
+app.post("/todo/delete/:id",(req, res) =>{
+    try{
+        const id = Number(req.params.id);
+        res.json(deleteNote(id));
+    } catch (err){
+        res.status(404).json({ error: err.message})
+    }
+    
 });
